3c104b65ff56b3ca3abcf2b7e20ecba641a65e35
nicolasmarini <nicolas.dario.marini@gmail.com>
2022-04-21 15:27:10 -0300

    Added enum files for ship types and ship ids

---
+++ src/enums/shipIds.js
@@ -0,0 +1,7 @@
+export default shipIds = {
+  carrier: "carrier",
+  cruiser1: "cruiser1",
+  cruiser2: "cruiser2",
+  cruiser3: "cruiser3",
+  submarine: "submarine"
+}
---
+++ src/enums/shipTypes.js
@@ -0,0 +1,5 @@
+export default shipTypes = {
+  carrier: "carrier",
+  cruiser: "cruiser",
+  submarine: "submarine"
+}
